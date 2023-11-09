92fa7695ec22ec0aa147ae4f6a45488cda0f7cce
Zal-Atan <ethanzalta@gmail.com>
2023-11-09 12:15:35 -0600

    Fixing pycodestyles 1 and 2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
+#!/usr/bin/python3
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def dashboard():
 
     return ("Hello HBNB!")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,16 +1,20 @@
+#!/usr/bin/python3
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def dashboard():
 
     return ("Hello HBNB!")
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
 
-    return("HBNB")
+    return ("HBNB")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
